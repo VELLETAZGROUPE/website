30b08b18c97d672b394d58674e6a50f692c624d2
Julien Fayard <julien@huitquatre.fr>
2024-07-18 23:33:36 +0200

    add sanity and duotone on Hero component

--- sanity-studio/schemaTypes/cmp/hero.js
+++ sanity-studio/schemaTypes/cmp/hero.js
@@ -23,5 +23,53 @@ export default {
         'Un lien vers /slug/#ancre peut être utilisé pour naviguer directement à cet endroit.',
       type: 'string',
     },
+    {
+      name: 'heading',
+      title: 'Titre',
+      type: 'object',
+      fields: [
+        {
+          name: 'content',
+          title: 'Texte',
+          type: 'string',
+        },
+        {
+          name: 'level',
+          title: 'Niveau hierarchique',
+          type: 'string',
+          options: {
+            list: [
+              {title: 'H1', value: 'h1'},
+              {title: 'H2', value: 'h2'},
+              {title: 'H3', value: 'h3'},
+              {title: 'H4', value: 'h4'},
+              {title: 'H5', value: 'h5'},
+              {title: 'H6', value: 'h6'},
+              {title: 'p', value: 'p'},
+            ],
+          },
+        },
+      ],
+    },
+    {
+      name: 'subheading',
+      title: 'Sous-titre',
+      description:
+        "Vous pouvez utiliser %year% pour afficher le nombre d'année écoulée depuis la création de l'entreprise (1987)",
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'img',
+      title: 'Image',
+      type: 'image',
+      initialValue: {
+        duotone: true,
+      },
+      fields: [
+        {name: 'alt', type: 'string'},
+        {name: 'duotone', type: 'boolean'},
+      ],
+    },
   ],
 }
