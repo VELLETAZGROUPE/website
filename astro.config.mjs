8826664adf09aa0f83737848da69d7e8493e0641
Julien Fayard <julien@huitquatre.fr>
2024-12-03 19:04:30 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.groupe-velletaz.com/",
+  site: "https://groupe-velletaz.com/",
   integrations: [tailwind(), sitemap()],
 });
