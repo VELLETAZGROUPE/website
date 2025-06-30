f180e5bf3637ff97b3dabc5883321f01eadfa79d
huitquatredev <julien@huitquatre.fr>
2025-06-30 14:24:01 +0200

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import criticalCss from "astro-critical-css";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://groupe-velletaz.com/",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), criticalCss()],
 });
